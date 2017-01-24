56f0243646e2d5b63aacd626ec83cbf7cc0157b0
niufenjujuexianhua <wutuo123@yeah.net>
2017-01-23 20:49:13 -0500

    Add new file

---
+++ Reverse_Interger.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 19:44:06 2017
+
+@author: twu
+
+if get time, rewrite the code. try to make it as concise as possible
+"""
+
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """               
+        sign = -1 if x < 0 else 1
+        x = x*sign
+        
+        y = 0
+        while x > 0:
+            y = y * 10 + x % 10
+            x = x//10
+        
+        if y > 0x7FFFFFFF:
+            return 0
+
+        return y*sign
+        
+        
+if __name__ == '__main__':
+    result = Solution().reverse(-12340)
+    print(result)
+        
