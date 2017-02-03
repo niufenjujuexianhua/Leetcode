b4186bde44b51c5e87480833e8bc85a41e16c6ea
niufenjujuexianhua <wutuo123@yeah.net>
2017-02-03 11:00:02 -0500

    add this file

---
+++ Count and Say.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 23:21:07 2017
+
+@author: twu
+"""
+
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        for i in range(n):
+            
+    
+
+
+
+    
+        
+if __name__ == '__main__':
+    result = Solution().countAndSay([1,3,5],2)
+    print(result)
