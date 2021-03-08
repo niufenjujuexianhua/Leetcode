4e8d75f74c00399c92ce99d59c7664a3f5926f25
niufenjujuexianhua <wutuo123@yeah.net>
2021-03-08 09:42:34 -0500

    Add files via upload

--- lcode.py
+++ lcode.py
@@ -1,57 +1,14 @@
 class Solution:
     """
-    @param target: a target string
-    @param dictionary: a set of strings in a dictionary
-    @return: an abbreviation of the target string with the smallest possible length
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
     """
-
-    def minAbbreviation(self, target, dictionary):
+    def minMeetingRooms(self, intervals):
         # Write your code here
-        import heapq
-        dictionary = [cand for cand in dictionary if len(cand) == len(target)]
-        q = []
-        self.bt(target, 0, 0, '', q)
+        order = sorted([(interval.start, 1) for interval in intervals] + [(interval.end, 0) for interval in intervals])
 
-        while q:
-            noconflict = True
-            _, abbr = heapq.heappop(q)
-            for word in dictionary:
-                if self.validWordAbbreviation(word, abbr):
-                    noconflict = False
-                    break
-            if noconflict:
-                return abbr
-
-    def bt(self, word, i, cnt, path, res):
-        import heapq
-        if i == len(word):
-            path += ['', str(cnt)][cnt != 0]
-            heapq.heappush(res, (len(path), path))
-            return
-
-        self.bt(word, i + 1, cnt + 1, path, res)
-        self.bt(word, i + 1, 0, path + ['', str(cnt)][cnt != 0] + word[i], res)
-
-    def validWordAbbreviation(self, word, abbr):
-        # write your code here
-        m, n = len(word), len(abbr)
-        i, j = 0, 0
-
-        while i < m and j < n:
-            if abbr[j].isdigit():
-                if abbr[j] == '0': return False
-
-                k = j
-                while k < n and abbr[k].isdigit(): k += 1
-                num = int(abbr[j: k])
-                j = k
-                i += num
-            else:
-                if word[i] != abbr[j]: return False
-                i += 1
-                j += 1
-
-        return i == m and j == n
-
-
-print(Solution().minAbbreviation("apple",["plain","amber","blade"]))
+        res = cnt = 0
+        for time, type in order:
+            cnt += [-1, 1][type == 1]
+            res = max(res, cnt)
+        return res 
