3ae88f148b9cda81d48227c8b48037e66a2a9627
niufenjujuexianhua <wutuo123@yeah.net>
2021-04-01 13:44:39 -0400

    Add files via upload

---
+++ [1283]Find the Smallest Divisor Given a Threshold.py
@@ -0,0 +1,77 @@
+# Given an array of integers nums and an integer threshold, we will choose a pos
+# itive integer divisor, divide all the array by it, and sum the division's result
+# . Find the smallest divisor such that the result mentioned above is less than or
+#  equal to threshold. 
+# 
+#  Each result of the division is rounded to the nearest integer greater than or
+#  equal to that element. (For example: 7/3 = 3 and 10/2 = 5). 
+# 
+#  It is guaranteed that there will be an answer. 
+# 
+#  
+#  Example 1: 
+# 
+#  
+# Input: nums = [1,2,5,9], threshold = 6
+# Output: 5
+# Explanation: We can get a sum to 17 (1+2+5+9) if the divisor is 1. 
+# If the divisor is 4 we can get a sum of 7 (1+1+2+3) and if the divisor is 5 th
+# e sum will be 5 (1+1+1+2). 
+#  
+# 
+#  Example 2: 
+# 
+#  
+# Input: nums = [44,22,33,11,1], threshold = 5
+# Output: 44
+#  
+# 
+#  Example 3: 
+# 
+#  
+# Input: nums = [21212,10101,12121], threshold = 1000000
+# Output: 1
+#  
+# 
+#  Example 4: 
+# 
+#  
+# Input: nums = [2,3,5,7,11], threshold = 11
+# Output: 3
+#  
+# 
+#  
+#  Constraints: 
+# 
+#  
+#  1 <= nums.length <= 5 * 104 
+#  1 <= nums[i] <= 106 
+#  nums.length <= threshold <= 106 
+#  
+#  Related Topics Binary Search 
+#  👍 712 👎 128
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def smallestDivisor(self, nums, threshold):
+        """
+        :type nums: List[int]
+        :type threshold: int
+        :rtype: int
+        """
+        s, e = 1, max(nums)
+        while s + 1 < e:
+            m = s + (e - s) // 2
+            if sum([(n - 1) // m + 1 for n in nums]) <= threshold:
+                e = m
+            else:
+                s = m
+        if sum([(n - 1) // s + 1 for n in nums]) <= threshold:
+            return s
+        return e
+
+    
+# print(Solution().smallestDivisor([21212,10101,12121],
+# 1000000))
+        # leetcode submit region end(Prohibit modification and deletion)
