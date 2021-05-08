1ecdbd0c9e15e103241ccc8d54d01d5749b29e24
niufenjujuexianhua <wutuo123@yeah.net>
2021-05-08 14:54:59 -0400

    Time: 868 ms (98.53%), Space: 23.8 MB (100.00%) - LeetHub

--- jump-game-vi/jump-game-vi.py
+++ jump-game-vi/jump-game-vi.py
@@ -6,13 +6,14 @@ class Solution(object):
         :rtype: int
         """
         import collections
-        dq = collections.deque([[0, nums[0]]])
-        for i, n in enumerate(nums[1:], 1):
-            if dq and dq[0][0] + k < i:
-                dq.popleft()
-            cur = dq[0][1] + n
-            while dq and cur >= dq[-1][1]:
-                dq.pop()
-            dq.append([i, cur])
-        return dq[-1][1]
-# print(Solution().maxResult([1,2,3], 4))
+        ln = len(nums)
+        bf = collections.deque([0])
+        for i in range(1, ln):
+            if bf and bf[0] + k < i:
+                bf.popleft()
+            cur = nums[bf[0]] + nums[i]
+            while bf and cur > nums[bf[-1]]:
+                bf.pop()
+            nums[i] = cur
+            bf.append(i)
+        return nums[bf[-1]]
