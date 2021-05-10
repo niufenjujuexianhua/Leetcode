cd6f9b88b2f34218f1d6be575ab3f75f6d5f13ab
niufenjujuexianhua <wutuo123@yeah.net>
2021-05-10 13:13:39 -0400

    Time: 1440 ms (40.38%), Space: 131.7 MB (25.41%) - LeetHub

--- count-primes/count-primes.py
+++ count-primes/count-primes.py
@@ -6,12 +6,13 @@ class Solution(object):
         """
         if n <= 1:
             return 0
-        cnt = [1] * n
-        cnt[0] = cnt[1] = 0
-        for i in range(2, n):
-            if cnt[i] == 1:
-                j = i
-                while i * j < n:
-                    cnt[i * j] = 0
-                    j += 1
-        return sum(cnt)
+        primes = [1] * n
+        primes[0] = primes[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if primes[i]:
+                for j in range(i*i, n, i):
+                    primes[j] = 0
+
+        return sum(primes)
+        
+# leetcode submit region end(Prohibit modification and deletion)
