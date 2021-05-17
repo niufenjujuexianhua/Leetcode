c744de1dcac5f70c23a7ae1099105fb28adf1d7b
niufenjujuexianhua <wutuo123@yeah.net>
2021-05-17 12:07:45 -0400

    Time: 120 ms (55.04%), Space: 14.1 MB (31.12%) - LeetHub

--- longest-string-chain/longest-string-chain.py
+++ longest-string-chain/longest-string-chain.py
@@ -1,26 +1,6 @@
 class Solution(object):
     def longestStrChain(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
-        wordsset = set(words)
-        m = {}
-        for word in wordsset:
-            self.dfs(wordsset, word, m)
-        res = max(m.values())
-        return res 
-
-    def dfs(self, wordsset, word, m):
-        if word not in wordsset:
-            return 0
-        if word in m:
-            return m[word]
-
-        ans = 0
-        for i in range(len(word)):
-            nword = word[:i] + word[i + 1:]
-            ans = max(ans, self.dfs(wordsset, nword, m))
-
-        m[word] = ans + 1
-        return m[word]
+        dp = {}
+        for w in sorted(words, key=len):
+            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in xrange(len(w)))
+        return max(dp.values())
