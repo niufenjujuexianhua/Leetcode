c81af5993911cb8e2470552e2f2588074f613152
niufenjujuexianhua <wutuo123@yeah.net>
2021-07-29 12:52:58 -0400

    Time: 432 ms (68.15%), Space: 38.9 MB (8.69%) - LeetHub

--- maximum-performance-of-a-team/maximum-performance-of-a-team.py
+++ maximum-performance-of-a-team/maximum-performance-of-a-team.py
@@ -1,11 +1,20 @@
 class Solution(object):
     def maxPerformance(self, n, speed, efficiency, k):
-        h = []
-        res = sSum = 0
-        for e, s in sorted(zip(efficiency, speed), reverse=1):
-            heapq.heappush(h, s)
-            sSum += s
-            if len(h) > k:
-                sSum -= heapq.heappop(h)
-            res = max(res, sSum * e)
-        return res % (10**9 + 7)
+        """
+        :type n: int
+        :type speed: List[int]
+        :type efficiency: List[int]
+        :type k: int
+        :rtype: int
+        """
+        hq = []
+        res = ss = 0 
+        for e, s in sorted([(e, s) for e, s in zip(efficiency, speed)], reverse = 1):
+            heapq.heappush(hq, s)
+            ss += s 
+            
+            if len(hq) > k:
+                ss -= heapq.heappop(hq)
+            
+            res = max(res, ss * e)
+        return res % (10 ** 9 + 7)
