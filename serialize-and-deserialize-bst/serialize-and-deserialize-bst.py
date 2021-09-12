ccf13c672b496164a37bf3da66cf57e0804a4944
niufenjujuexianhua <wutuo123@yeah.net>
2021-09-12 14:04:06 -0400

    Time: 84 ms (41.20%), Space: 21.1 MB (87.50%) - LeetHub

--- serialize-and-deserialize-bst/serialize-and-deserialize-bst.py
+++ serialize-and-deserialize-bst/serialize-and-deserialize-bst.py
@@ -1,51 +1,39 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Codec:
 
     def serialize(self, root):
         """Encodes a tree to a single string.
-        
+
         :type root: TreeNode
         :rtype: str
         """
-        return self.preorder(root)
-        
+        # print(','.join(map(str, self.preorder(root))))
+        if not root:
+            return ''
+        return ','.join(map(str, self.preorder(root)))
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
-        
+
         :type data: str
         :rtype: TreeNode
         """
-        return self.deser(collections.deque(data.split(',')))
+        if not data:
+            return [] 
+        return self.deser(collections.deque(data.split(',')), float('-inf'), float('inf'))
 
+    def deser(self, ls, lo, hi):
+        if ls and lo < int(ls[0]) < hi:
+            # root = int(ls.popleft())
+            # if not lo < root < hi:
+            #     return None
 
-    def deser(self, ls):
-        if ls:
-            root = ls.popleft()
-            if root == '#':
-                return None
-
-            root = TreeNode(root)
-            root.left = self.deser(ls)
-            root.right = self.deser(ls)
+            root = TreeNode(int(ls.popleft()))
+            root.left = self.deser(ls, lo, root.val)
+            root.right = self.deser(ls, root.val, hi)
             return root
 
     def preorder(self, root):
         if not root:
-            return '#'
+            return []
 
-        return str(root.val) + ',' + self.preorder(root.left) + ',' + self.preorder(root.right)
-        
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# tree = ser.serialize(root)
-# ans = deser.deserialize(tree)
-# return ans
+        return [root.val] + self.preorder(root.left) + self.preorder(root.right)
