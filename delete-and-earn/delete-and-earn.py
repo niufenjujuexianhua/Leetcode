af4404b4cf4c8e7e70e3e043bc046983b4ac9d43
niufenjujuexianhua <wutuo123@yeah.net>
2021-10-06 20:30:43 -0400

    Time: 44 ms (67.24%), Space: 13.7 MB (50.00%) - LeetHub

--- delete-and-earn/delete-and-earn.py
+++ delete-and-earn/delete-and-earn.py
@@ -3,14 +3,26 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
+        3 1 1 3 1
+        1 2 5 6
+        3 3 7 18 13
+
+        p[i] = max(p[i - 1], p[i - 2] + cur * freq)
         """
-        mx = max(nums)
-        dp = [0] * (mx + 1)
+        import collections
         freq = collections.Counter(nums)
+        take, notake = 0, 0
+        prev = None 
 
-        for i in range(1, mx + 1):
-            rob = dp[i - 2] + freq[i] * i if i >= 2 else freq[i] * i
-            notrob = dp[i - 1]
-            dp[i] = max(rob, notrob)
+        for n in sorted(freq):
+            f = freq[n]
+            tmptake, tmpnotake = take, notake
+            if n - 1 != prev:
+                take = n * f + max(tmptake, tmpnotake)
+            else:
+                take = n * f + tmpnotake
 
-        return dp[-1]
+            notake = max(tmptake, tmpnotake)
+            prev = n 
+
+        return max(take, notake)
