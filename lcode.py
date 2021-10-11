52e1a708ec956bddbf267ccddac04f1c6e80db05
niufenjujuexianhua <wutuo123@yeah.net>
2021-10-10 23:42:51 -0400

    Add files via upload

--- lcode.py
+++ lcode.py
@@ -1,14 +1,38 @@
 class Solution:
     """
-    @param intervals: an array of meeting time intervals
-    @return: the minimum number of conference rooms required
+    @param costs: n x k cost matrix
+    @return: an integer, the minimum cost to paint all houses
+    1 2 3
+    4 5 6
+    7 8 9
+    dp[i][j] = costs[i][j] + min(dp[i - 1][k] k != j)
     """
-    def minMeetingRooms(self, intervals):
-        # Write your code here
-        order = sorted([(interval.start, 1) for interval in intervals] + [(interval.end, 0) for interval in intervals])
 
-        res = cnt = 0
-        for time, type in order:
-            cnt += [-1, 1][type == 1]
-            res = max(res, cnt)
-        return res 
+    def minCostII(self, costs):
+        # write your code here
+        if not costs or len(costs) == 0:
+            return 0
+        n, k = len(costs), len(costs[0])
+        if n == 0:
+            return 0
+
+        min1 = min2 = 0
+        dp = [0] * k
+
+        for i in range(n):
+            omin1, omin2 = min1, min2
+            min1 = min2 = float('inf')
+
+            for j in range(k):
+                if dp[j] != omin1 or omin1 == omin2:
+                    dp[j] = omin1 + costs[i][j]
+                else:
+                    dp[j] = omin2 + costs[i][j]
+
+                if min1 <= dp[j]:
+                    min2 = min(min2, dp[j])
+                else:
+                    min2 = min1
+                    min1 = dp[j]
+        return min1
+
