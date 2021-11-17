e395c93a23201ec0cbdff1c6d5244ca4aa441d39
niufenjujuexianhua <wutuo123@yeah.net>
2021-11-17 14:05:18 -0500

    Time: 20 ms (50.06%), Space: 13.7 MB (6.34%) - LeetHub

--- binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -9,17 +9,23 @@ class Solution(object):
         """
         :type root: TreeNode
         :rtype: List[int]
-        """        
-        res = []
-        self.dfs(root, res)
-        return res 
-        
-    def dfs(self, root, res):
-        if not root:
-            return 
-        
-        
-        self.dfs(root.left, res)
-        self.dfs(root.right, res)
-        res.append(root.val)
+        """
+        node, st, res = root, [], []
+
+        while node or st:
+            while node:
+                st.append(node)
+                if node.left:
+                    node = node.left
+                else:
+                    node = node.right
+
+            node = st.pop()
+            res.append(node.val)
+            if st and st[-1].left == node:
+                node = st[-1].right
+            else:
+                node = None
+
+        return res
         
