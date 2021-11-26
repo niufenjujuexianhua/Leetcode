8177cd0c316c6b005ce164e1627655e89606262f
niufenjujuexianhua <wutuo123@yeah.net>
2021-11-26 12:08:23 -0500

    Time: 512 ms (95.35%), Space: 24.5 MB (60.89%) - LeetHub

---
+++ contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        dt = {}
+        for i, n in enumerate(nums):
+            if n in dt and i - dt[n] <= k:
+                return True
+            dt[n] = i 
+        return False
