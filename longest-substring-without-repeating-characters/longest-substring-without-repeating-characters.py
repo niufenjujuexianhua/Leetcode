3967c861058d9fdd18d093deb6b0395e32f83fd1
niufenjujuexianhua <wutuo123@yeah.net>
2021-11-27 17:42:17 -0500

    Time: 56 ms (60.54%), Space: 13.9 MB (50.76%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -4,20 +4,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dt = collections.defaultdict(int)
         i = res = dup = 0
-
+        dt = collections.defaultdict(int)
+        
         for j, ch in enumerate(s):
-            dt[ch] += 1
+            dt[ch] += 1 
             if dt[ch] == 2:
-                dup += 1
-
-            while dup > 0 and i < len(s):
-                dt[s[i]] -= 1
+                dup += 1 
+                
+            while dup > 0:
+                dt[s[i]] -= 1 
                 if dt[s[i]] == 1:
-                    dup -= 1
-                i += 1
-
+                    dup -= 1 
+                i += 1 
+            
             res = max(res, j - i + 1)
-
-        return res
+        return res 
