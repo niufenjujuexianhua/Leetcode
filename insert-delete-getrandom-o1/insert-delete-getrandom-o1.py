019ea8234f6c2eb51ba8a4ff4fd7590983837bff
niufenjujuexianhua <wutuo123@yeah.net>
2021-11-27 18:23:59 -0500

    Time: 516 ms (73.79%), Space: 58.8 MB (88.91%) - LeetHub

--- insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,43 +1,44 @@
 class RandomizedSet(object):
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.ls = []
+        self.ls = [] 
         self.pos = {}
+        
 
     def insert(self, val):
         """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
         :type val: int
         :rtype: bool
         """
         if val not in self.pos:
+            self.pos[val] = len(self.pos)
             self.ls.append(val)
-            self.pos[val] = len(self.ls) - 1
             return True
         return False
         
 
     def remove(self, val):
         """
-        Removes a value from the set. Returns true if the set contained the specified element.
         :type val: int
         :rtype: bool
         """
         if val in self.pos:
-            i = self.pos[val]
-            self.ls[i], self.ls[len(self.ls) - 1] = self.ls[len(self.ls) - 1], self.ls[i]
-            self.pos[self.ls[i]] = i
-            del self.pos[self.ls[-1]]
+            ival = self.pos[val]
+            
+            ilast, last = len(self.ls) - 1, self.ls[-1]
+            
+            self.ls[ival] = last 
+            self.pos[last] = ival 
+            
             self.ls.pop()
+            del self.pos[val]
             return True
+        
         return False
+        
 
     def getRandom(self):
         """
-        Get a random element from the set.
         :rtype: int
         """
         import random
