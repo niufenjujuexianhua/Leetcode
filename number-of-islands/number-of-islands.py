f6cedf534258f947701b94fb2673c8b82ef6d0e7
niufenjujuexianhua <wutuo123@yeah.net>
2021-12-16 10:09:41 -0500

    Time: 373 ms (25.30%), Space: 28.8 MB (73.22%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,30 +1,27 @@
 class Solution(object):
-    def numIslands(self, rooms):
+    def numIslands(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not rooms or not rooms[0]:
-            return
-        m, n = len(rooms), len(rooms[0])
-        res = 0 
+        m, n = len(grid), len(grid[0])
+        res = 0
+
         for i in range(m):
             for j in range(n):
-                if rooms[i][j] == '1':
-                    res += 1 
-                    self.bfs(rooms, m, n, i, j, 0)
-                    # rooms[i][j] = dist
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j, m, n)
+                    res += 1
         return res
 
-    def bfs(self, rooms, m, n, i, j, d):
-        if not (0 <= i < m and 0 <= j < n) or rooms[i][j] == '0':
+
+    def dfs(self, grid, i, j, m, n):
+        if grid[i][j] == '0':
             return
-        # if d > rooms[i][j]:
-        #     return
 
-        rooms[i][j] = '0'
+        grid[i][j] = '0'
 
-        self.bfs(rooms, m, n, i + 1, j, d + 1)
-        self.bfs(rooms, m, n, i, j + 1, d + 1)
-        self.bfs(rooms, m, n, i - 1, j, d + 1)
-        self.bfs(rooms, m, n, i, j - 1, d + 1)
+        for di, dj in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
+            ni, nj = i + di, j + dj
+            if 0 <= ni < m and 0 <= nj < n:
+                self.dfs(grid, ni, nj, m, n)
