15813e9bb42497e33b70adf64b22b71e2adcaf80
niufenjujuexianhua <wutuo123@yeah.net>
2021-12-16 17:10:30 -0500

    Time: 24 ms (31.56%), Space: 13.5 MB (53.73%) - LeetHub

--- evaluate-division/evaluate-division.py
+++ evaluate-division/evaluate-division.py
@@ -6,30 +6,40 @@ class Solution(object):
         :type queries: List[List[str]]
         :rtype: List[float]
         """
+        import collections
         g = collections.defaultdict(dict)
 
-        for i, (a, b) in enumerate(equations):
-            g[a][b] = values[i]
-            g[b][a] = 1.0 / values[i]
+        for i in range(len(equations)):
+            a, b = equations[i]
+            v = values[i]
+            g[a][b] = v
+            g[b][a] = 1.0 / v
 
         res = []
-        for s, d in queries:
-            v = self.dfs(g, s, d, 1.0, set())
+        for s, e in queries:
+            if s in g and e in g[s]:
+                res.append(g[s][e])
+                continue
+
+            v = self.dfs(g, s, e, 1.0, set())
             if v != -1:
-                g[s][d] = v
-                g[d][s] = 1.0 / v
+                g[s][e] = v
+                g[e][s] = 1.0 / v
             res.append(v)
         return res
 
-    def dfs(self, g, s, d, path, seen):
-        if s not in g or d not in g or s in seen:
-            return -1.0
-        if s == d:
-            return path
 
+    def dfs(self, g, s, e, path, seen):
+        if s not in g or e not in g or s in seen:
+            return -1
+
+        if s == e:
+            return path
+        
         seen.add(s)
         for nxt in g[s]:
-            tmp = self.dfs(g, nxt, d, path * g[s][nxt], seen)
-            if tmp != -1.0:
-                return tmp
-        return -1.0
+            # tmp = g[s][nxt]
+            val = self.dfs(g, nxt, e, path * g[s][nxt], seen)
+            if val != -1:
+                return val 
+        return -1 
