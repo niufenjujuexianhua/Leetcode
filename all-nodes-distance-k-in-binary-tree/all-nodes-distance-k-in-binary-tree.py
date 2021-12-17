4a769edd2d72a4d6cb61e50748f60261d21b1afa
niufenjujuexianhua <wutuo123@yeah.net>
2021-12-17 16:18:16 -0500

    Time: 23 ms (79.94%), Space: 13.8 MB (79.62%) - LeetHub

--- all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -1,37 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
-    def distanceK(self, root, target, k):
+    def distanceK(self, root, target, K):
         """
         :type root: TreeNode
         :type target: TreeNode
         :type K: int
         :rtype: List[int]
         """
-        g = collections.defaultdict(list)
-        self.build(root, g)
-        # print(g)
-        res, seen, q = [], {target.val}, collections.deque([(target.val, 0)])
+        self.res = []
+        self.dfs(root, target, K)
+        return self.res
 
-        while q:
-            node, dis = q.popleft()
-            # print(node, dis)
-            if dis == k:
-                res.append(node)
-            if dis + 1 <= k:
-                for nei in g[node]:
-                    if nei not in seen:
-                        seen.add(nei)
-                        q.append((nei, dis + 1))
-        return res
-
-    def build(self, root, g):
-        if not root:
+    def fetch(self, node, k):
+        #add node at dist = k
+        if not node or k < 0:
             return
+        if k == 0:
+            self.res.append(node.val)
+            return
+        self.fetch(node.left, k - 1)
+        self.fetch(node.right, k - 1)
 
-        if root.left:
-            g[root.val].append(root.left.val)
-            g[root.left.val].append(root.val)
-        if root.right:
-            g[root.val].append(root.right.val)
-            g[root.right.val].append(root.val)
-        self.build(root.left, g)
-        self.build(root.right, g)
+
+    def dfs(self, node, target, k):
+        if not node:
+            return -1
+        if node == target:
+            self.fetch(node, k)
+            return 0
+
+        lt = self.dfs(node.left, target, k)
+        if lt != -1:
+            if lt == k - 1:
+                self.res.append(node.val)
+            else:
+                self.fetch(node.right, k - lt - 2)
+            return lt + 1
+
+        rt = self.dfs(node.right, target, k)
+        if rt != -1:
+            if rt == k - 1:
+                self.res.append(node.val)
+            else:
+                self.fetch(node.left, k - rt - 2)
+            return rt + 1
+
+        return -1
+        
