e961df767aff1d062d03bb76fd53d1f1943a7b68
niufenjujuexianhua <wutuo123@yeah.net>
2021-12-20 13:28:56 -0500

    Time: 80 ms (90.09%), Space: 14.9 MB (60.72%) - LeetHub

--- cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,38 +1,16 @@
 class Solution(object):
-    def findCheapestPrice(self, n, flights, src, dst, k):
-        """
-        :type n: int
-        :type flights: List[List[int]]
-        :type src: int
-        :type dst: int
-        :type k: int
-        :rtype: int
-        """
-        import collections
-        if not flights or src == dst:
-            return 0
-
-        g = collections.defaultdict(dict)
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        visited = {}
+        graph = collections.defaultdict(list)
         for s, d, p in flights:
-            g[s][d] = p
-        # return self.bfs(n, flights, src, dst, k)
-
-        # res = [float('inf')]
-        # self.dfs(n, flights, src, dst, -1, k, res, 0, g)
-        # return res[0]if res[0] != float('inf') else -1
-        # return self.dijstra(n, flights, src, dst, k, g)
-
-        return self.bellman(n, flights, src, dst, k, g)
-    def bellman(self, n, flights, src, dst, k, g):
-        cost = [float('inf')] * n
-        cost[src] = 0
-        for i in range(k + 1):
-            cur = cost[:]
-
-            for s in g:
-                for d in g[s]:
-                    if cost[s] != float('inf') and cost[s] + g[s][d] < cur[d]:
-                        cur[d] = cost[s] + g[s][d]
-            cost = cur[:]
-
-        return cost[dst] if cost[dst] != float('inf') else -1
+            graph[s].append((d, p))
+        heap = [(0, 0, src)]
+        while heap:
+            dist, moves, node = heapq.heappop(heap)
+            if node == dst and K >= moves - 1:
+                return dist
+            if node not in visited or visited[node] > moves:
+                visited[node] = moves
+                for nei, weight in graph[node]:
+                    heapq.heappush(heap, (dist + weight, moves + 1, nei))
+        return -1
