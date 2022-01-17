0e9a406c1255324cfe5840df8f60698408d0980d
niufenjujuexianhua <wutuo123@yeah.net>
2022-01-16 23:34:49 -0500

    Time: 327 ms (5.02%), Space: 16 MB (53.56%) - LeetHub

--- 529-minesweeper/529-minesweeper.py
+++ 529-minesweeper/529-minesweeper.py
@@ -4,31 +4,37 @@ class Solution(object):
         :type board: List[List[str]]
         :type click: List[int]
         :rtype: List[List[str]]
+        for di, dj in [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]:
         """
         m, n = len(board), len(board[0])
-
-        if board[click[0]][click[1]] == 'M':
-            board[click[0]][click[1]] = 'X'
+        i, j = click
+        if board[i][j] == 'M':
+            board[i][j] = 'X'
             return board
 
-        self.dfs(board, click[0], click[1])
+        self.dfs(board, i, j)
         return board
 
+
     def dfs(self, board, i, j):
-        if not (0 <= i < len(board) and 0 <= j < len(board[0])) or board[i][j] != 'E':
+        # if board[i][j] == 'M':
+        #     board[i][j] = 'X'
+        #     return
+        if board[i][j] != 'E':
             return
 
         cnt = 0
-        for di, dj in [(-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0)]:
+        for di, dj in [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]:
             ni, nj = i + di, j + dj
-            if 0 <= ni < len(board) and 0 <= nj < len(board[0]) and board[ni][nj] == 'M':
-                cnt += 1
+            if 0 <= ni < len(board) and 0 <= nj < len(board[0]):
+                if board[ni][nj] == 'M':
+                    cnt += 1
 
         if cnt == 0:
             board[i][j] = 'B'
             for di, dj in [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]:
                 ni, nj = i + di, j + dj
-                self.dfs(board, ni, nj)
+                if 0 <= ni < len(board) and 0 <= nj < len(board[0]):
+                    self.dfs(board, ni, nj)
         else:
             board[i][j] = str(cnt)
-            return
