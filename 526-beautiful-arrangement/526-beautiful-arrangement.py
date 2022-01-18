3be3eaf3a68feaf86d944b1f08cb2e0b0c97a893
niufenjujuexianhua <wutuo123@yeah.net>
2022-01-18 10:58:54 -0500

    Time: 1820 ms (36.45%), Space: 13.5 MB (36.00%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countArrangement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        seen = [0] * n
+        res = [0]
+        self.dfs(seen, 0, 0, res)
+        return res[0]
+
+    def dfs(self, seen, i, j, res):
+        if j == len(seen):
+            res[0] += 1
+            return
+
+        for k in range(len(seen)):
+            if not seen[k]:
+                if (k + 1) % (j + 1) == 0 or (j + 1) % (k + 1) == 0:
+                    seen[k] = 1
+                    self.dfs(seen, i, j + 1, res)
+                    seen[k] = 0
