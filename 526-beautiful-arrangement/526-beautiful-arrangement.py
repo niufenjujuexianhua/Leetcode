1c4303d215f9c64766f4de7501730f1ed0e5f155
niufenjujuexianhua <wutuo123@yeah.net>
2022-01-18 10:59:38 -0500

    Time: 2655 ms (16.01%), Space: 13.4 MB (81.33%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.py
+++ 526-beautiful-arrangement/526-beautiful-arrangement.py
@@ -6,10 +6,10 @@ class Solution(object):
         """
         seen = [0] * n
         res = [0]
-        self.dfs(seen, 0, 0, res)
+        self.dfs(seen, 0, res)
         return res[0]
 
-    def dfs(self, seen, i, j, res):
+    def dfs(self, seen, j, res):
         if j == len(seen):
             res[0] += 1
             return
@@ -18,5 +18,5 @@ class Solution(object):
             if not seen[k]:
                 if (k + 1) % (j + 1) == 0 or (j + 1) % (k + 1) == 0:
                     seen[k] = 1
-                    self.dfs(seen, i, j + 1, res)
+                    self.dfs(seen, j + 1, res)
                     seen[k] = 0
