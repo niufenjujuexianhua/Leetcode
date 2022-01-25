1f26c7d27c1ba30d517cc573e1c6b19b1a7eec3b
niufenjujuexianhua <wutuo123@yeah.net>
2022-01-25 11:51:57 -0500

    Time: 216 ms (59.62%), Space: 20.9 MB (51.34%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.py
+++ 721-accounts-merge/721-accounts-merge.py
@@ -1,25 +1,3 @@
-class UnionFind():
-    def __init__(self, n):
-        self.p = list(range(n))
-        self.rank = [0] * n
-
-    def find(self, x):
-        if self.p[x] != x:
-            self.p[x] = self.find(self.p[x])
-        return self.p[x]
-
-    def union(self, x, y):
-        px, py = self.find(x), self.find(y)
-        if px != py:
-            if self.rank[px] > self.rank[py]:
-                self.p[py] = px
-            elif self.rank[px] < self.rank[py]:
-                self.p[px] = py
-            else:
-                self.p[px] = py
-                self.rank[py] += 1
-        return
-
 class Solution(object):
     def accountsMerge(self, accounts):
         """
@@ -27,26 +5,27 @@ class Solution(object):
         :rtype: List[List[str]]
         """
         import collections
-        uf = UnionFind(len(accounts))
-        e2id = collections.defaultdict(list)
+        dt = collections.defaultdict(set)
 
         for i, acc in enumerate(accounts):
-            for j in range(1, len(acc)):
-                e2id[acc[j]].append(i)
-
-        for em, accs in e2id.items():
-            for i in range(1, len(accs)):
-                uf.union(accs[0], accs[i])
-
-        dt = collections.defaultdict(set)
-        for i in range(len(accounts)):
-            dt[uf.find(i)].add(i)
+            for em in acc[1:]:
+                dt[em].add(i)
 
         res = []
-        for accs in dt.values():
-            accs = list(accs)
-            emails = set()
-            for i in accs:
-                emails |= set(accounts[i][1:])
-            res.append([accounts[accs[0]][0]] + sorted(emails))
+        seen = set()
+        for i in range(len(accounts)):
+            if i not in seen:
+                emails = set()
+                self.dfs(accounts, i, seen, dt, emails)
+
+                res.append([accounts[i][0]] + sorted(emails))
         return res
+
+    def dfs(self, accounts, i, seen, dt, emails):
+        if i in seen:
+            return
+        seen.add(i)
+        for em in accounts[i][1:]:
+            emails.add(em)
+            for j in dt[em]:
+                self.dfs(accounts, j, seen, dt, emails)
