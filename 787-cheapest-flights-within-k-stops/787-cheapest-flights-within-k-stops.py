5f3db84b66f3b40d1899f9dd976e309ee0f771d9
niufenjujuexianhua <wutuo123@yeah.net>
2022-02-20 17:01:42 -0500

    Time: 80 ms (96.12%), Space: 15 MB (49.32%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -4,20 +4,20 @@ class Solution(object):
         for f, t, p in flights:
             neis[f].append((t, p))
             
-        heap = [(0, k, src)]
+        heap = [(0, -1, src)]
         seen_stops = collections.defaultdict(int)
 
         while heap:
             totalP, stops, city = heapq.heappop(heap)
             if city == dst:
                 return totalP
-            if stops < 0:
+            if stops == k:
                 continue
-            if city in seen_stops and seen_stops[city] >= stops:
+            if city in seen_stops and seen_stops[city] <= stops:
                 continue
             seen_stops[city] = stops
             
             for nei, neiP in neis[city]:
-                heapq.heappush(heap, (totalP + neiP, stops - 1, nei))
+                heapq.heappush(heap, (totalP + neiP, stops + 1, nei))
         
         return -1
