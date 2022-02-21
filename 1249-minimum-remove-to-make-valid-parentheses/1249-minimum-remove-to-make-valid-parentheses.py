49f7bb85dd82ba94af2e11d1e0b25322f16103ec
niufenjujuexianhua <wutuo123@yeah.net>
2022-02-21 10:42:21 -0500

    Time: 98 ms (93.01%), Space: 15.8 MB (75.44%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minRemoveToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ss, st = list(s), []
+        for i, ch in enumerate(ss):
+            if ch == ')':
+                if st: 
+                    st.pop()
+                else: 
+                    ss[i] = ''
+            elif ch == '(':
+                st.append(i)
+
+        for i in st:
+            ss[i] = ''
+        return ''.join(ss)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
